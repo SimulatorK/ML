1042e6b2bdb39e7b95d2635b13dd58dc1008610b
Mason Kelchner <mason.j.kelchner@gmail.com>
2023-08-29 20:08:42 -0500

    Add file

---
+++ assignment_1.py
@@ -0,0 +1,15 @@
+## CS7641 Machine Learning - Assignment 1
+"""
+    Create models for Decision Tree,DT with boosting, SVM, KNN, Neural Nets 
+
+
+
+
+
+"""
+
+from sklearn import preprocessing, utils
+import sklearn.model_selection as ms
+
+# Load data sets
+
