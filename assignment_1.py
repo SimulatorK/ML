e2ae16b8163e1a3164ae116822154d9f0cefe0cb
Mason Kelchner <mason.j.kelchner@boeing.com>
2023-08-31 09:21:41 -0500

    Update figure 1. Add additional DT tests.

--- assignment_1.py
+++ assignment_1.py
@@ -19,8 +19,10 @@ import pandas as pd
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
+
 # Load data sets
-random.seed(1)
+seed = 1
 
 if __name__ == '__main__':
     os.chdir(os.path.split(__file__)[0])
@@ -30,23 +32,50 @@ creditDefaultFile = r".\data\default of credit card clients.xls"
 ds1 = pd.read_excel(creditDefaultFile,skiprows=1)
 
 ###############################################################################
+# Helper Functions
+###############################################################################
+
+def vPrint(text: str = '', verbose: bool = True):
+    if verbose:
+        print(text)
+
+def timefit(model,args,verbose: bool = True):
+    start = time()
+    model.fit(*args)
+    end = time()
+    inter = end - start
+    st = '{:,.6}'.format(inter)
+    t = f'{model}: Fit Time: {st} seconds'
+    vPrint(t,verbose)
+    return inter
+
+###############################################################################
 # Decision Tree Test
 ###############################################################################
 dt_out_scores_random = []
 dt_in_scores_random = []
 dt_out_scores_best = []
 dt_in_scores_best = []
+dt_rand_time = []
+dt_best_time = []
 
-min_samples = np.linspace(200,2,10)
+# Min samples to try
+min_samples = list(map(int,np.linspace(200,2,10)))
+# Max depth to try
+max_depths = list(map(int,np.linspace(1,20,10)))
 
 ## Get X, Y data for test and train
 Xdata = ds1.iloc[:,1:-1]
 Ydata = ds1.iloc[:,-1]
 
-# Split data
+# Split data (set random seed)
 test_size = 0.2
+Xtrain, Xtest, Ytrain, Ytest = ms.train_test_split(Xdata,
+                                                   Ydata,
+                                                   test_size=test_size,
+                                                   random_state=seed)
 
-Xtrain, Xtest, Ytrain, Ytest = ms.train_test_split(Xdata, Ydata, test_size=test_size)
+## Loop for min samples
 for mss in min_samples:
     ## Create random decision tree 
     min_samples_leaf = int(mss)
@@ -54,14 +83,15 @@ for mss in min_samples:
     ccp_alpha = 0.0
     
     dt = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
-                                splitter='random')
+                                splitter='random',
+                                random_state = seed)
     # Print attributes
     print(f'\tRandom Decision Tree min_samples_leaf: {dt.min_samples_leaf}')
     print(f'\tRandom Decision Tree max depth: {dt.max_depth}')
     
     # Fit the dt to training data
-    dt.fit(Xtrain,Ytrain)
-    
+    t = timefit(dt,(Xtrain,Ytrain))
+    dt_rand_time.append(t)
     
     # Score on test data
     dtscore = dt.score(Xtest,Ytest)
@@ -75,14 +105,15 @@ for mss in min_samples:
     
     ## Decision tree with best split
     dt = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,
-                                splitter='best')
+                                splitter='best',
+                                random_state = seed)
     # Print attributes
     print(f'\tBestSplit Decision Tree min_samples_leaf: {dt.min_samples_leaf}')
     print(f'\tBestSplit Decision Tree max depth: {dt.max_depth}')
     
     # Fit the dt to training data
-    dt.fit(Xtrain,Ytrain)
-    
+    t = timefit(dt,(Xtrain,Ytrain))
+    dt_best_time.append(t)
     
     # Score on test data
     dtscore = dt.score(Xtest,Ytest)
@@ -92,33 +123,130 @@ for mss in min_samples:
     # Add to scores
     dt_out_scores_best.append(dtscore)
     dt_in_scores_best.append(dt.score(Xtrain,Ytrain))
+
+## Loop for max depth, fixed min_samples_leaf = 50
+dt_out_scores_random_md = []
+dt_in_scores_random_md = []
+dt_out_scores_best_md = []
+dt_in_scores_best_md = []
+dt_rand_time_md = []
+dt_best_time_md = []
+
+for md in max_depths:
+    ## Create random decision tree 
+    min_samples_leaf = 100
+    max_depth = md
+    ccp_alpha = 0.0
+    
+    dt = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,max_depth=max_depth,
+                                splitter='random',
+                                random_state = seed)
+    # Print attributes
+    print(f'\tRandom Decision Tree min_samples_leaf: {dt.min_samples_leaf}')
+    print(f'\tRandom Decision Tree max depth: {dt.max_depth}')
+    
+    # Fit the dt to training data
+    t = timefit(dt,(Xtrain,Ytrain))
+    dt_rand_time_md.append(t)
+    
+    # Score on test data
+    dtscore = dt.score(Xtest,Ytest)
+    
+    print(f'\tRandom Decision Tree Score: {dtscore}')
+    
+    # Add to scores
+    dt_out_scores_random_md.append(dtscore)
+    dt_in_scores_random_md.append(dt.score(Xtrain,Ytrain))
+    
+    
+    ## Decision tree with best split
+    dt = DecisionTreeClassifier(min_samples_leaf=min_samples_leaf,max_depth=max_depth,
+                                splitter='best',
+                                random_state = seed)
+    # Print attributes
+    print(f'\tBestSplit Decision Tree min_samples_leaf: {dt.min_samples_leaf}')
+    print(f'\tBestSplit Decision Tree max depth: {dt.max_depth}')
+    
+    # Fit the dt to training data
+    timefit(dt,(Xtrain,Ytrain))
+    dt_best_time_md.append(t)
+    
+    # Score on test data
+    dtscore = dt.score(Xtest,Ytest)
+    
+    print(f'\tBestSplit Decision Tree Score: {dtscore}')
+    
+    # Add to scores
+    dt_out_scores_best_md.append(dtscore)
+    dt_in_scores_best_md.append(dt.score(Xtrain,Ytrain))
     
 ## Create plot
-fig, ax =  plt.subplots(1,2,figsize=(6,4),dpi=200)
+fig, ax =  plt.subplots(2,2,figsize=(12,6),dpi=200)
+## Plot fit times on second axis for each tree
 
-#Random Split
-ax[0].plot(min_samples,dt_in_scores_random,label='Training Score')
-ax[0].plot(min_samples,dt_out_scores_random,label='Test Score')
-    
-ax[0].set_xlabel('min_samples_leaf')
-ax[0].set_ylabel('Score')
-ax[0].grid()
-ax[0].legend()
+#Random Split Min_samples
+ax00 = ax[0][0].twinx()
+l1 = ax[0][0].plot(min_samples,dt_in_scores_random,label='Training Score')
+l2 = ax[0][0].plot(min_samples,dt_out_scores_random,label='Test Score')
+l3 = ax00.plot(min_samples,dt_rand_time,'k--',label='Training Time')
+ax00.set_ylabel('Training Time')
+ls = l1+ l2 + l3
+lb = [l.get_label() for l in ls]
+ax[0][0].set_xlabel('min_samples_leaf')
+ax[0][0].set_ylabel('Score')
+ax[0][0].grid()
+ax[0][0].legend(ls,lb,loc=0)
 
-# Best Split
-ax[1].plot(min_samples,dt_in_scores_best,label='Training Score')
-ax[1].plot(min_samples,dt_out_scores_best,label='Test Score')
-    
-ax[1].set_xlabel('min_samples_leaf')
-ax[1].set_ylabel('Score')
-ax[1].grid()
-ax[1].legend()
+# Best Split Min_samples
+ax01 = ax[0][1].twinx()
+l1 = ax[0][1].plot(min_samples,dt_in_scores_best,label='Training Score')
+l2 = ax[0][1].plot(min_samples,dt_out_scores_best,label='Test Score')
+l3 = ax01.plot(min_samples,dt_best_time,'k--',label = 'Training Time')
+ax[0][1].set_xlabel('min_samples_leaf')
+ax01.set_ylabel('Training Time')
+ax[0][1].set_ylabel('Score')
+ax[0][1].grid()
+ls = l1 + l2 + l3
+lb = [l.get_label() for l in ls]
+ax[0][1].legend(ls,lb,loc=0)
 
+#Random Split Max_depths
+ax10 = ax[1][0].twinx()
+l1 = ax[1][0].plot(max_depths,dt_in_scores_random_md,label='Training Score')
+l2 = ax[1][0].plot(max_depths,dt_out_scores_random_md,label='Test Score')
+l3 = ax10.plot(max_depths,dt_rand_time_md,'k--',label='Training Time')
+ax10.set_ylabel('Training Time')
+ls = l1 + l2 + l3
+lb = [l.get_label() for l in ls]
+ax[1][0].set_xlabel(f'Max_depth (min_samples={min_samples_leaf})')
+ax[1][0].set_ylabel('Score')
+ax[1][0].grid()
+ax[1][0].legend(ls,lb,loc=0)
+
+# Best Split Max_depths
+ax11 = ax[1][1].twinx()
+l1 = ax[1][1].plot(max_depths,dt_in_scores_best_md,label='Training Score')
+l2 = ax[1][1].plot(max_depths,dt_out_scores_best_md,label='Test Score')
+l3 = ax11.plot(max_depths,dt_best_time_md,'k--',label='Training Time')
+ax11.set_ylabel('Training Time')
+ls = l1 + l2 + l3
+lb = [l.get_label() for l in ls]
+ax[1][1].set_xlabel(f'Max_depth (min_samples={min_samples_leaf})')
+ax[1][1].set_ylabel('Score')
+ax[1][1].grid()
+ax[1][1].legend(ls,lb,loc=0)
+
+ax[0][0].set_title('Random Splitter')
+ax[0][1].set_title('Best Feature Splitter')
+
+fig.tight_layout()
+plt.suptitle('Decision Tree Classifier Hyperparameter Test')
 plt.savefig('DecisionTreeClassifier_Figure.png')
 plt.show() # Save fig
 
     
 ##### Decision Tree with Boosting
+n_estimators = 100
 dt_boost = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,
                                       max_depth=1, random_state=0)
 
